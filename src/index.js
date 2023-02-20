d9b7d78c59923f4852b88dc711cfa0e6e7c9a04e
Eugene-prog <ieugene.prog@gmail.com>
2023-02-20 22:30:02 +0700

    feat: add solve hard task barckets!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const OPEN_BRACKETS = bracketsConfig.map((item) => item[0]);
+  const BRACKETS_PAIR = Object.fromEntries(bracketsConfig.map((item) => [item[1], item[0]]));
+
+  const isBracketsOk = (str) => {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      let topElement = stack[stack.length - 1];
+
+      if (
+        BRACKETS_PAIR[currentSymbol] !== undefined &&
+        OPEN_BRACKETS.includes(currentSymbol) &&
+        currentSymbol === topElement
+      ) {
+        stack.pop();
+      } else if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
+  };
+
+  return isBracketsOk(str);
+};
